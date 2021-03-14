ac64500f1c008874b999e3f11d3fc21bd597cd7a
Shaw Malcolm <shawmalcolm@protonmail.com>
2021-03-14 12:29:22 +0000

    add container and fix DOM manipulation

--- scripts.js
+++ scripts.js
@@ -1,26 +1,26 @@
-const button = document.querySelector("button");
-const printHex = document.querySelector(".printHex h2");
-const hexValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
-let hexVal = "";
+const container = document.querySelector('.container')
+const button = document.querySelector('button')
+const printHex = document.querySelector('.printHex h2')
+const hexValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F']
+let hexVal = ''
 
 function changeGradient() {
-  //change background of body to a randomly generated gradient
-  let hexOne = "#";
-  let hexTwo = "#";
+  let hexOne = '#'
+  let hexTwo = '#'
 
   for (let i = 0; i < 6; i++) {
-    const index = Math.floor(Math.random() * hexValues.length);
-    hexOne += hexValues[index];
+    const index = Math.floor(Math.random() * hexValues.length)
+    hexOne += hexValues[index]
   }
 
   for (let i = 0; i < 6; i++) {
-    const index = Math.floor(Math.random() * hexValues.length);
-    hexTwo += hexValues[index];
+    const index = Math.floor(Math.random() * hexValues.length)
+    hexTwo += hexValues[index]
   }
 
-  hexVal = `linear-gradient(45deg, ${hexOne}, ${hexTwo})`;
-  document.body.style.background = hexVal;
-  printHex.textContent = hexVal;
+  hexVal = `linear-gradient(45deg, ${hexOne}, ${hexTwo})`
+  container.style.background = hexVal
+  printHex.textContent = hexVal
 }
 
-button.addEventListener("click", changeGradient);
+button.addEventListener('click', changeGradient)
